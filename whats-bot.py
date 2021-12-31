369848f60b999ccc14ac0836a135faf84ea83355
Marcelo Camara <80636179+omarcelocamara@users.noreply.github.com>
2021-12-31 19:49:40 -0300

    Update whats-bot.py
    Apenas a adição de um comentário que eu havia esquecido de colocar.

--- whats-bot.py
+++ whats-bot.py
@@ -2,10 +2,10 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time 
 
-PATH = "C:\Program Files\chromedriver.exe"
+PATH = "C:\Program Files\chromedriver.exe" #Coloque a localização do seu arquivo do Chromedriver aqui.
 driver = webdriver.Chrome(PATH)
 
-driver.get("https://web.whatsapp.com/")
+driver.get("https://web.whatsapp.com/") 
 time.sleep(15)
 #escanear o QR Code para entrar no WhatsApp Web
 grupo1 = driver.find_element_by_xpath("//span[@title='NOME DO GRUPO1']")
